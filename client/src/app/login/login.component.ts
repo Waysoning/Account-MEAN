301cbd035773bcce05bbef23682aeef2b245997d
wayson <wayson1225@outlook.com>
2022-12-19 21:02:58 -0800

    FRONTEND: add login form

---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  login(user: any) {
+    console.log(this.loginForm.value);
+  }
+}
